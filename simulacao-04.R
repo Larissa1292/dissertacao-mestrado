fb3ad6140367f60fac615429147a614ffef56857
Larissa9212 <larissadsl12@gmail.com>
2019-06-26 17:22:30 -0300

    Testando R=2 e n=5000

--- simulacao-04.R
+++ simulacao-04.R
@@ -14,8 +14,8 @@ pi0 <- 0.1
 pi1 <- 0.2
 lambda <- 2
 sig <- 0.2
-R <- 1 #num de replicas de Monte Carlo
-n <- 1000 # tamanho da amostra
+R <- 2 #num de replicas de Monte Carlo
+n <- 5000 # tamanho da amostra
 #ytil <- matrix(0, nrow = n, ncol = R)
 #opt1 <- matrix(0, R, n) #col = num de parametros a serem estimados
 
