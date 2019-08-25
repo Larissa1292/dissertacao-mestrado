90ecb53e927231dd521c0914723f830345e4d2c8
larissa <larissa@rstudioserver.us-central1-a.c.rstudioserver-247313.internal>
2019-08-25 17:55:48 -0300

    Altera nome do arquivo com os resultados

--- simulacao-03-gera-m3-aplica-m3.R
+++ simulacao-03-gera-m3-aplica-m3.R
@@ -265,4 +265,4 @@ resultado <- list(
 resultado
 
 # Salvando os resultados em um arquivo
-write.csv(x = resultado, file = paste0("m3_r", R,"_n", n, "_l", lambda,"_sig", sig,".csv"))
+write.csv(x = resultado, file = paste0("m3_gera_r", R,"_n", n, "_l", lambda,"_sig", sig,".csv"))
