892884c1d3b21e96a4ed81f633079196e2a9a4c4
larissa <larissa@rstudioserver.us-central1-a.c.rstudioserver-247313.internal>
2019-11-01 15:07:24 -0300

    Teste com cod antigo

--- novoteste_m4.R
+++ novoteste_m4.R
@@ -23,16 +23,16 @@ sig <- as.numeric(argumentos[6]) # => sig² = 0.01
 R <- as.numeric(argumentos[7]) #num de replicas de Monte Carlo
 n <- as.numeric(argumentos[8]) # tamanho da amostra
 
-#### Definindo os parâmetros iniciais (para rodar direto pelo rstudio) ####
+#### Definindo os parâmetros iniciais ####
 
-# pi0 <- 0.1
-# pi1 <- 0.2
 # beta0 <- 0
 # beta1 <- 1
+# pi0 <- 0.1
+# pi1 <- 0.2
 # lambda <- 2
-# sig <- 0.001 # => sig² = 0.000001
-# R <- 500 #num de replicas de Monte Carlo
-# n <- 10000 # tamanho da amostra
+# sig <- 0.2
+# R <- 10 #num de replicas de Monte Carlo
+# n <- 1000# tamanho da amostra
 
 #### Vetor de parâmetros ####
 
@@ -48,8 +48,6 @@ inicio <- Sys.time()
 set.seed(1992)
 
 m4_loglik <- function(theta, w, y){
-  sig = 0.01
-  n = 1000
   
   #### Definindo expressões e valores para a esp.condicional ####
   
@@ -98,17 +96,15 @@ for(i in 1:R){
   #### Passo 1: Gerar w_i amostras da U(-4, 4) ####
   
   w <- runif(n,-4, 4)
-  print(Sys.time() - inicio)  
-  
+  print(Sys.time() - inicio)
   #### Passo 2: Gerar x_i amostras da Skew Normal ####
   
   x <- rsn(n = n, xi = w, omega = sig ^ 2, alpha = parametros[5])
-  print(Sys.time() - inicio)  
-  
+  print(Sys.time() - inicio)
   #### Passo 3: Gerar y_i da Bernoulli ####
   
   y <- rbinom(n = n, size = 1, prob = pnorm(parametros[3] + parametros[4] * x))
-  print(Sys.time() - inicio)  
+  print(Sys.time() - inicio)
   
   p.i <- ifelse(y == 0, pi0, pi1)
   
@@ -119,7 +115,7 @@ for(i in 1:R){
   #### Passo 4: Gerar Ytil ####
   
   ytil <- abs(comparacao - y)
-  print(Sys.time() - inicio)  
+  print(Sys.time() - inicio)
   
   #Comparar cada elemento da Uniforme com o vetor y em (pi0, pi1)
   
@@ -131,20 +127,17 @@ for(i in 1:R){
   #### Passo 5: otimizacao ####
   
   tryCatch(  {
-    otimizacao <- optimParallel(
-      #par = c(0.1, 0.2, 0, 1, 2), #chutes iniciais
-      par = c(0.03, 0.03, 0.001, 0.99, 0.0009),
-      fn = m4_loglik,
+    otimizacao <- optim(
+      par = c(0.1, 0.2, 0, 1, 2),
+      fn = m4_n,
       method = "L-BFGS-B",
       control = list(fnscale = -1),
       lower = c(0.000001, 0.000001,-Inf,-Inf,-Inf),
-      upper = c(0.99999999999, 0.99999999999, Inf, Inf, Inf),
-      w = w,
-      y = y
+      upper = c(0.99999999999, 0.99999999999, Inf, Inf, Inf)
     )
     ## O R faz minimização por default, então para maximizar devo usar "control=list(fnscale=-1)"
     
-    print(Sys.time() - inicio)
+    
     if (otimizacao$convergence == 0) { #0: indica convergencia completa
       emv.pi0[i] = otimizacao$par[1]
       emv.pi1[i] = otimizacao$par[2]
@@ -176,7 +169,6 @@ beta0sd <- sd(emv.beta0)
 beta1sd <- sd(emv.beta1)
 lambdasd <- sd(emv.lambda)
 
-
 #### Calculando viés e erro quadratico medio (eqm) ####
 # calculando o viés (vies = media - valor verdadeiro do parametro)
 pi0vies <- pi0medio - pi0
@@ -205,6 +197,7 @@ lambdaEQM <- var(emv.lambda) + (lambdavies) ^ 2
 fim <- Sys.time()
 tempo <- fim - inicio
 
+#### Lista com os resultados finais ####
 resultado <- list(
   Num_obs = n,
   Replicas = R,
@@ -218,11 +211,6 @@ resultado <- list(
   Beta0_est_medio = beta0medio,
   Beta1_est_medio = beta1medio,
   Lambda_est_medio = lambdamedio,
-  Pi0_sd = pi0sd,
-  P1_sd = pi1sd,
-  Beta0_sd = beta0sd,
-  Beta1_sd = beta1sd,
-  Lambda_sd = lambdasd,
   Pi0_est_vies = pi0vies,
   Pi1_est_vies = pi1vies,
   Beta0_est_vies = beta0vies,
@@ -244,6 +232,3 @@ resultado <- list(
 
 # Imprimindo os resultados
 resultado
-
-# Salvando os resultados em um arquivo
-write.csv(x = resultado, file = paste0("m4_r", R, "_n", n, "_l", lambda, "_sig", sig, ".csv"))
