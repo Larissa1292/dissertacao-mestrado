3569b9c0035281682a901d56fd3538d834fa9449
Larissa1292 <larissadsl12@gmail.com>
2019-11-29 14:46:48 +0000

    Ajusta tamanho de amostra e sig para pi0 = pi1 = 0.01, beta0 = 0, beta1 = 1, lambda = 0.001, sig2 = 0.01

--- simulacao-04.R
+++ simulacao-04.R
@@ -50,8 +50,8 @@ inicio <- Sys.time()
 set.seed(1992)
 
 m4_loglik <- function(theta, w, y){
-  sig = 0.2
-  n = 20000
+  sig = 0.1
+  n = 10000
   #### Definindo expressões e valores para a esp.condicional ####
   
   #theta <- c(0.1, 0.2, 0, 1, 2) #vetor para testar sem precisar rodar a funcao m4
