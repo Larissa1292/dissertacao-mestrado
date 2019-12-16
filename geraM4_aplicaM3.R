2323b27841ea3f600d54d8b32f2b7d5fb2fa56bf
Larissa Lima <larissadsl12@gmail.com>
2019-12-16 15:07:06 +0000

    Ajusta nome do arquivo de saída dos resultados

--- geraM4_aplicaM3.R
+++ geraM4_aplicaM3.R
@@ -239,4 +239,4 @@ resultado <- list(
 resultado
 
 # Salvando os resultados em um arquivo
-write.csv(x = resultado, file = paste0("m4_r", R, "_n", n, "_l", lambda, "_sig", sig, ".csv"))
+write.csv(x = resultado, file = paste0("geram4_aplicam3_r", R, "_n", n, "_l", lambda, "_sig", sig, ".csv"))
