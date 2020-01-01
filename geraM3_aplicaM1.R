f2fdea00c0ec8794838e1530e90893437471b6c1
Larissa Lima <larissadsl12@gmail.com>
2020-01-01 12:15:16 +0000

    Ajusta chutes iniciais para esquema 2

--- geraM3_aplicaM1.R
+++ geraM3_aplicaM1.R
@@ -120,7 +120,9 @@ for(i in 1:R){
   
   tryCatch(  {
     otimizacao <- optimParallel(
-      par = c(0.001, 0.99, 0.01),
+      #par = c(0.001, 0.99, 0.01),
+      #par = c(0.001, 0.99, 0.0009), #chutes iniciais para esquema 1 (sig=0.1)
+      par = c(0.001, 0.99, 0.0009), #chutes iniciais para esquema 2 (sig=0.7)
       fn = m3_loglik,
       method = "L-BFGS-B",
       control = list(fnscale = -1),
